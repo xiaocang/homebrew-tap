2adbd154d2fd60cc0f22814723c864b8d1d98e0d
Johnny Wang <johnnywang1991@msn.com>
2026-03-21 09:42:47 +0800

    Update PRDashboard Cask URL and homepage

--- Casks/prdashboard.rb
+++ Casks/prdashboard.rb
@@ -2,10 +2,10 @@ cask "prdashboard" do
   version "1.1.4"
   sha256 "4333bf985ad1a160ad9561b88cc43063ec5925531602140c2aac4c59225c0b0d"
 
-  url "https://github.com/xiaocang/ghpr-view/releases/download/v#{version}/PRDashboard-#{version}.zip"
+  url "https://github.com/ghpr-lab/ghpr-view/releases/download/v#{version}/PRDashboard-#{version}.zip"
   name "PRDashboard"
   desc "Menu bar app to track GitHub Pull Requests and unresolved comments"
-  homepage "https://github.com/xiaocang/ghpr-view"
+  homepage "https://github.com/ghpr-lab/ghpr-view"
 
   app "PRDashboard.app"
 
